866999e09d5fcffc7e44d1c1b52108319636ca31
yanjun <kingfisherzh@gmail.com>
2020-06-16 04:17:18 +0900

    3rd update

--- Week4/Q1-Dijkstra.py
+++ Week4/Q1-Dijkstra.py
@@ -1,36 +1,46 @@
+from collections import defaultdict
 
 
 def Dijkstra(node_dist, current_node, target):
 	not_visited = [_ for _ in range(num_station)]
-	shortest_dist = [999 for _ in range(num_station)]		# Initialize the distance
+	shortest_dist = {}
+	for i in range(num_station):
+		shortest_dist[str(i)] = 999							# Initialize the distance
 	shortest_path = [[] for _ in range(num_station)]		# Record paths
-	shortest_dist[int(current_node)] = 0					# Distance from start node is 0
+	shortest_dist[str(current_node)] = 0					# Distance from start node is 0
+	record = []
 
 	while not_visited:
 		if current_node in not_visited:
 			if node_dist[str(current_node)]:
 				for node in node_dist[str(current_node)].keys():
-					if shortest_dist[int(node)] > shortest_dist[current_node] + node_dist[str(current_node)][node]:
-						shortest_dist[int(node)] = shortest_dist[current_node] + node_dist[str(current_node)][node]
+					if shortest_dist[str(node)] > shortest_dist[str(current_node)] + node_dist[str(current_node)][node]:
+						shortest_dist[str(node)] = shortest_dist[str(current_node)] + node_dist[str(current_node)][node]
 						for prev_node in shortest_path[int(current_node)]:
 							shortest_path[int(node)].append(prev_node)
 						shortest_path[int(node)].append(str(current_node))
-				
-			shortest_copy = shortest_dist.copy()	
-			not_visited.remove(int(current_node))
-			current_node = shortest_copy.index(min(shortest_copy))
+						record.append([str(node), shortest_dist[str(node)]])
 
+
+			not_visited.remove(int(current_node))
+
+			record = sorted(record, key=lambda record:record[1])
+			current_node = record[0][1]
+			count = 0
 			# Find the node with the shortest distance as the next current node
 			# and meanwhile this node should have not been visited yet
-			# We replace the distance in the unsatisfied current node with 999
-			while current_node not in not_visited:
-				shortest_copy[current_node] = 999
-				current_node = shortest_copy.index(min(shortest_copy))
-				# All nodes have been visited
-				if shortest_copy == [999 for _ in range(250)]:
+			
+			while int(current_node) not in not_visited:
+				if count == len(record)-1:
 					print('shortest_path',shortest_path[target])
-					print('distance', shortest_dist[target])
-					return 
+					print('distance', shortest_dist[str(target)])
+					return
+				else:	 
+					count += 1
+					current_node = int(record[count][0])
+					
+			
+				
 
 if __name__ == '__main__':
 	f = open('edges.txt')
@@ -38,7 +48,7 @@ if __name__ == '__main__':
 	num_station = 250
 	node_dist = {}
 	for i in range(num_station):
-		node_dist[str(i)] = {}
+		node_dist[str(i)] = defaultdict(dict)
 
 	for line in lines:
 		start = line.split('\t')[0]
